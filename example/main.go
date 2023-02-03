47906925eabda8f7653b3911c90277f1809bb273
twiddlebits <28405084+bytetwiddler@users.noreply.github.com>
2023-02-02 16:00:25 -0800

    added minimal example as the old one was a main()   of the test code

--- example/main.go
+++ example/main.go
@@ -4,55 +4,25 @@ import (
 	"fmt"
 
 	v "github.com/bytetwiddler/aba-routingnumber-validator"
-	"github.com/jedib0t/go-pretty/table"
 )
-
-type test struct {
-	value       string
-	expect      bool
-	description string
-}
-
-func tablePrint(tw table.Writer) {
-	// append a header row
-	tw.AppendHeader(table.Row{"PASS/FAIL:\u2713/\u2717", "Input", "Expected", "Received", "Failure description"})
-	// render table
-	fmt.Printf("Unit Results:\n%s\n", tw.Render())
-}
 
 func main() {
 
-	// setup our test table
-	testTable := []test{
-		{"123123123", true, ""},
-		{"122105278", true, ""},
-		{"022105278", false, "check sum fail"},
-		{"12345678", false, "short 8 digits"},
-		{"1234567890", false, "long 10 digits"},
-		{"934567890", false, "1st 2 digits not between 01-12"},
-		{"134567890", false, "1st 2 digits not between 01-12"},
-	}
+	// ignoring errors for clarity
 
-	fmt.Println(`Note: errors are just printed out and not handled as we are passing test data
-and expect errors. Check the tables PASS/FAIL field to see if a particular 
-routing number got the expected result.
-	 `)
-	tw := table.NewWriter()
-	for _, t := range testTable {
-		slc, err := v.StringSlicer(t.value)
-		if err != nil {
-			fmt.Println("error: ", err)
-		}
-		result, err := v.ValidateAbaRoutingNumber(slc)
-		if err != nil {
-			fmt.Println("error: ", err)
-		}
-		if result == t.expect {
-			tw.AppendRow(table.Row{"\u2713", t.value, t.expect, result, t.description})
-		} else {
-			tw.AppendRow(table.Row{"\u2717", t.value, t.expect, result, t.description})
-		}
-	}
+	// turn string into slice of slice []int required by
+	// ValidateAbaRoutingNumber().
+	//
+	// note that you could just generate a slice []int yourself
+	// and pass to ValidateAbaRoutingNumber(). StringSlicer is
+	// provided as a convience as folks get stumped by go
+	// assuming 012312312 is an out of bound octal.
+	// this is a valid routing number
+	slice, _ := v.StringSlicer("123123123")
 
-	tablePrint(tw)
+	// ValidateAbaRoutingNumber is the only require function
+	// it takes a slice of in []int and returns a bool, error
+	res, _ := v.ValidateAbaRoutingNumber(slice)
+
+	fmt.Println(res)
 }
