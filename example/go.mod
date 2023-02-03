3afa643a70e045bbe3acbc9ccde100d159b6ccd1
twiddlebits <28405084+bytetwiddler@users.noreply.github.com>
2023-02-03 09:35:33 -0800

    Changed module name

--- example/go.mod
+++ example/go.mod
@@ -1,7 +1,7 @@
-module example-aba-routingnumber-validator
+module example-aba-routing-number-validator
 
 go 1.19
 
-require github.com/bytetwiddler/aba-routingnumber-validator v0.0.0-20230202193648-8a0f9d31d74b
+require github.com/bytetwiddler/aba-routing-number-validator v0.0.0-20230202193648-8a0f9d31d74b
 
-replace github.com/bytetwiddler/aba-routingnumber-validator => ../validator
+replace github.com/bytetwiddler/aba-routing-number-validator => ../validator
--- example/main.go
+++ example/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 
-	v "github.com/bytetwiddler/aba-routingnumber-validator"
+	v "github.com/bytetwiddler/aba-routing-number-validator"
 )
 
 func main() {
 
 	// ignoring errors for clarity
 
-	// note: that you could just generate a slice of []int 
-	// and pass to ValidateAbaRoutingNumber(). 
+	// note: that you could just generate a slice of []int
+	// and pass to ValidateAbaRoutingNumber().
 	// StringSlicer function is provided as a convience as
 	// folks get stumped by golang assuming 012312312 is an
 	// out of bound octal.
 	// 123123123 is a valid routing number.
-	
+
 	// ValidateAbaRoutingNumber()
 	// turns string version of the number into slice of []int
 	slice, _ := v.StringSlicer("123123123")
